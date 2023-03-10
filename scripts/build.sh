b70536d59e482d85e69256f455b038791ebcaf58
吴勇 <wuychn@163.com>
2023-03-10 14:00:59 +0800

    修改org/opensearch为com/colasoft/opensearch

--- scripts/build.sh
+++ scripts/build.sh
@@ -78,5 +78,5 @@ mkdir -p $OUTPUT/plugins
 cp ${distributions}/*.zip ./$OUTPUT/plugins
 
 ./gradlew publishPluginZipPublicationToZipStagingRepository -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
-mkdir -p $OUTPUT/maven/org/opensearch
-cp -r ./build/local-staging-repo/org/opensearch/. $OUTPUT/maven/org/opensearch
+mkdir -p $OUTPUT/maven/com/colasoft/opensearch
+cp -r ./build/local-staging-repo/com/colasoft/opensearch/. $OUTPUT/maven/com/colasoft/opensearch
